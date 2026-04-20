23f9186157db58fe6d9f2db80e52beac8c5b2576
saranya <saranyadhuruvan2021@gmail.com>
2026-04-19 22:12:24 -0400

    new eks file 1

--- main.tf
+++ main.tf
@@ -22,8 +22,8 @@ data "aws_vpc" "default" {
 
 data "aws_subnets" "default" {
   filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+    name   = "availability-zone"
+    values = ["us-east-1a", "us-east-1b", "us-east-1c"]
   }
 }
 
